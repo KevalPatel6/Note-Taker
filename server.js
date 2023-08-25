a754774e44da3b018e02dc862af1c189e7e5ed6c
[Keval Patel] <[khp90611@gmail.com]>
2023-08-24 23:56:55 -0400

    changed port to see if it works

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const notesPage = require('./routes/notesPage.js')
 const notesRouter = require('./routes/apiNotes.js')
 const path = require('path')
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
