4dece8a0a99af80e596606cff10589375742a5e4
[Keval Patel] <[khp90611@gmail.com]>
2023-08-24 22:12:24 -0400

    Made some minor changes to delete request. It is actually not working. Need to work on it but running out of time so I am going to do the README next before deploying

--- public/routes/apiNotes.js
+++ public/routes/apiNotes.js
@@ -44,16 +44,18 @@ notesRouter.post('/', (req, res) => {
 
 notesRouter.delete('/:id', (req,res)=>{
     const id = req.params.id
-    console.log(id)
-    console.log(notes.length)
     for (let i = 0; i < notes.length; i++) {
         if(id === notes[i].id){
-            const spliced = notes.splice(i,1)
-           fs.writeFile(path.join(__dirname,'../../db/db.json'), spliced, (err)=> 
+            console.log(notes)
+            const splicedArr = notes.splice(i-1,1)
+            console.log(splicedArr)
+            const splicedArrString = JSON.stringify(splicedArr)
+            console.log(splicedArrString)
+           fs.writeFile(path.join(__dirname,'../../db/db.json'), splicedArrString, (err)=> 
            err  
                 ? console.log(err)
                 : console.log(
-                    `Deleted ${note[i].title} from database`
+                    `Deleted note from database`
                 ))
         }        
        
